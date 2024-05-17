adb492de46b09229bf52b411eabe7b1a65428e67
Joe Porcelli <joe@kt3i.com>
2024-05-16 22:02:29 -0400

    Pulled port, host, and debug config out of app and into config.py

--- app.py
+++ app.py
@@ -571,5 +571,5 @@ def parse_time(str):
 
 if __name__ == '__main__':
     create_database()
-    app.run(debug=True, host="0.0.0.0", port="8080")
+    app.run(Config.DEBUG, host=Config.HOST, port=Config.PORT)
     
