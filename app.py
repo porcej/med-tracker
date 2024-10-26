926d8757ecdfa46e772f2eb8f6852dd78b1fd600
Joseph Porcelli <porcej@gmail.com>
2024-10-26 11:00:09 -0400

    Fixed variable name

--- app.py
+++ app.py
@@ -582,7 +582,7 @@ def connect():
         'key': Config.UPSTREAM_KEY,
         'room': encounters
     }
-    sio.emit('join', msg, namespace="/sync")
+    remote_sio.emit('join', msg, namespace="/sync")
 
 @remote_sio.event(namespace="/sync")
 def disconnect():
